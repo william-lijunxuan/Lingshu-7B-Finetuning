47ae6fad9dbea76e062f5151d321e95361493892
william <mr.william.li@foxmail.com>
2026-05-09 14:28:51 +0800

    modify Qwen3VL_2b model config

--- qwen3vl/Qwen3VL_2b.py
+++ qwen3vl/Qwen3VL_2b.py
@@ -252,7 +252,7 @@ training_args = GRPOConfig(
     use_vllm=True,
     vllm_mode="colocate",
     vllm_gpu_memory_utilization=0.65,  # 0.30
-    vllm_max_model_length=12288,
+    vllm_max_model_length=8192,
 
     dataloader_num_workers=8,
 
