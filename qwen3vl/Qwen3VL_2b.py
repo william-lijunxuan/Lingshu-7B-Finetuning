56dbcb9d0bdd9cf6aef5a8c894892540d01582cd
william <mr.william.li@foxmail.com>
2026-05-09 13:05:38 +0800

    modify Qwen3VL_2b model config

--- qwen3vl/Qwen3VL_2b.py
+++ qwen3vl/Qwen3VL_2b.py
@@ -200,7 +200,7 @@ training_args = GRPOConfig(
     max_steps=3400,                                        # Number of dataset passes. For full trainings, use `num_train_epochs` instead
     # num_train_epochs=3,
     # Parameters that control the data preprocessing
-    per_device_train_batch_size=4,
+    per_device_train_batch_size=8,
     max_completion_length=64, # default: 256            # Max completion length produced during training
     num_generations=8, # 2, # default: 8                  # Number of generations produced during training for comparison
 
